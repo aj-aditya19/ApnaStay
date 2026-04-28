a1f159ce17eba29aac469883b900d03f26dd6008
aj-aditya19 <ajaditya1908@gmail.com>
2026-04-28 10:50:09 +0530

    hosted version 1.0

--- init/index.js
+++ init/index.js
@@ -2,20 +2,30 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/ApnaStay";
+
 main()
   .then((res) => {
     console.log("Database Connection Successful");
   })
   .catch((err) => {
     console.log("Some error occured in the database");
+    console.error(err);
   });
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/ApnaStay");
+  await mongoose.connect(dbUrl);
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
-  initData.data = initData.data.map((obj)=>({...obj, owner: "698b313ef0e74bb54c49604a"}));
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "698b313ef0e74bb54c49604a",
+  }));
   await Listing.insertMany(initData.data);
   console.log("Data was initialized");
 };
